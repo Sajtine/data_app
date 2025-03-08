386f27bbb2e80f8c930b298b69858a08187bfe08
Justine Gomez <justinegomez777@gmail.com>
2025-03-08 08:35:44 +0800

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-
-http.createServer((req, res) => {
-    fs.readFile("index.html", (err, data) => {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    });
-}).listen(2000, () => console.log("Server running on port 2000"));
